3a53c7aa14e056cc439581e7bc556ceda5c1e87a
eason-pan-ep <eason.pan@proton.me>
2025-02-25 21:17:14 -0800

    move to the correct file

---
+++ solutions/p0217_contains_duplicate.py
@@ -0,0 +1,17 @@
+"""
+LeetCode #217: Contains Duplicate
+Difficulty: Easy
+Topics: Array, Set
+URL: https://leetcode.com/problems/contains-duplicate/
+"""
+
+from typing import List
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    check_set = set()
+    for num in nums:
+        if num in check_set:
+            return True
+        check_set.add(num)
+    
+    return False
