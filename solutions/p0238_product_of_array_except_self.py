1bbfa8f54623191f1d1949eb0c73c3efde9709fc
eason-pan-ep <eason.pan@proton.me>
2025-03-05 08:58:28 -0800

    add a solution using prefix and postfix product

--- solutions/p0238_product_of_array_except_self.py
+++ solutions/p0238_product_of_array_except_self.py
@@ -10,7 +10,7 @@ from typing import List
 from collections import Counter
 
 class Solution:
-    def productExceptSelf_solution_1(self, nums: List[int]) -> List[int]:
+    def product_except_self_solution_1(self, nums: List[int]) -> List[int]:
         """Solution using division"""
         nums_count = Counter(nums)
         # if there is no zero, have a product of every element and each will be the all product / current num
@@ -42,3 +42,20 @@ class Solution:
                     res[i] = non_zero_product
                     break
             return res
+    
+    def product_except_self_solution_2(self, nums: List[int]) -> List[int]:
+        """Solution using prefix and postfix product"""
+        res = [1] * len(nums)
+        # fill item-i with its prefix product
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        # multiply item-i with its postfix product
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
