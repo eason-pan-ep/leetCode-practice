06fda805914c05001778cdaa04b5db5b64db6e38
eason-pan-ep <eason.pan@proton.me>
2025-03-14 17:21:45 -0700

    add 2 more solutions for p80

--- solutions/p0080_remove_duplicates_from_sorted_array_ii.py
+++ solutions/p0080_remove_duplicates_from_sorted_array_ii.py
@@ -10,6 +10,37 @@ from typing import List
 from collections import Counter
 
 class Solution:
+    def remove_duplicates_solution_3(self, nums: List[int]) -> int:
+        """solution to use two pointers, O(n) time and O(1) space"""
+        left, right = 0, 0
+
+        while right < len(nums):
+            if left < 2 or nums[right] != nums[left - 2]:
+                nums[left] = nums[right]
+                left += 1
+            right += 1
+        return left
+    
+    
+    def remove_duplicates_solution_2(self, nums: List[int]) -> int:
+        """solution to use two pointers, O(n) time and O(1) space"""
+        n = len(nums)
+        if n <= 2:
+            return n
+        
+        left, right = 0, 0
+        while right < len(nums):
+            count = 1
+            while (right + 1 < n) and (nums[right] == nums[right+1]):
+                right += 1
+                count += 1
+            for i in range(min(2, count)):
+                nums[left] = nums[right]
+                left += 1
+            right += 1
+        return left
+
+
     def remove_duplicates_solution_1(self, nums: List[int]) -> int:
         """Solution to use hash map, O(n) time and O(n) space"""
         n = len(nums)
