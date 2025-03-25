9e79a17243fefebb410eb0e69a8af3812cb9e67e
eason-pan-ep <eason.pan@proton.me>
2025-03-24 17:07:58 -0700

    add second solution

--- solutions/p0015_3_sum.py
+++ solutions/p0015_3_sum.py
@@ -9,6 +9,40 @@ URL: https://leetcode.com/problems/3sum/
 from typing import List
 
 class Solution:
+    def three_sum_solution_2(self, nums: List[int]) -> List[List[int]]:
+        """Two pointers, time: O(n^2), space: O(n)"""
+        n = len(nums)
+        if n < 3:
+            return []
+
+        res = []
+        nums.sort()
+        for index, num in enumerate(nums):
+            # if the smallest is already greater than 0, there is no chance to sum up equals to 0
+            if num > 0:
+                break
+            
+            # the same num as the previous one, ans will be the same
+            if index > 0 and num == nums[index-1]:
+                continue
+            
+            left, right = index + 1, n - 1
+            while left < right:
+                cur_sum = num + nums[left] + nums[right]
+                if cur_sum == 0:
+                    res.append([num, nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while nums[left] == nums[left-1] and left < right:
+                        left += 1
+                elif cur_sum > 0:
+                    right -= 1
+                else:
+                    left += 1
+            
+        return res
+
+
     def three_sum_solution_1(self, nums: List[int]) -> List[List[int]]:
         """Brute force, time: O(n^3), space: O(n)"""
         n = len(nums)
