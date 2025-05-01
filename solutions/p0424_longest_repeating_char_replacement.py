f35c64b4a9915b78f2ff8a628ea57a6c880453c5
eason-pan-ep <eason.pan@proton.me>
2025-05-01 16:14:10 -0700

    add solution 2

--- solutions/p0424_longest_repeating_char_replacement.py
+++ solutions/p0424_longest_repeating_char_replacement.py
@@ -7,8 +7,35 @@ URL: https://leetcode.com/problems/longest-repeating-character-replacement/
 """
 
 class Solution:
+    def character_replacement_solution_2(self, s: str, k: int) -> int:
+        """
+        O(n) time complexity, O(m) space complexity, 
+        where m is the number of unique characters in s and n is the length of s.
+        """
+        count = {}
+        highest_freq = 0
+        max_len = 0
+        left = 0
+
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            highest_freq = max(highest_freq, count[s[right]])
+
+
+            while (right - left + 1) - highest_freq > k:  # when characters need replacement are more than the limit
+                count[s[left]] -= 1
+                left += 1
+
+            max_len = max(right - left + 1, max_len)
+        
+        return max_len
+    
+    
     def character_replacement_solution_1(self, s: str, k: int) -> int:
-        """O(m*n) time complexity, O(n) space complexity, where m is the number of unique characters in s and n is the length of s."""
+        """
+        O(m*n) time complexity, O(n) space complexity, 
+        where m is the number of unique characters in s and n is the length of s.
+        """
         chars = set(s)
         max_len = 0
 
